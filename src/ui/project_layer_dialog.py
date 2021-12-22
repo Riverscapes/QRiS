cd7278e0b3aed55a6ef92c66ad6f77c0ebec2d94
Nick Weber <webernick@icloud.com>
2021-12-22 15:34:39 -0800

    Adds clip layers to project extent functionality

--- src/ui/project_layer_dialog.py
+++ src/ui/project_layer_dialog.py
@@ -3,6 +3,8 @@ from PyQt5.QtWidgets import QMessageBox
 
 from osgeo import gdal
 
+from qgis import processing
+
 from qgis.PyQt import uic
 from qgis.PyQt.QtWidgets import QDialog, QDialogButtonBox, QDialogButtonBox
 from qgis.PyQt.QtCore import pyqtSignal
@@ -40,8 +42,15 @@ class ProjectLayerDlg(QDialog, DIALOG_CLASS):
             self.lineEdit_feature_name.setText("Create New Layer")
 
         self.set_valid_layer_name()
-
         self.lineEdit_display_name.textChanged.connect(self.set_valid_layer_name)
+
+        # setup the clip combobox
+        extent_layer_list = []
+        for extent in self.qris_project.project_extents.values():
+            extent_layer_list.append(extent.display_name)
+        self.comboBox_clip_extent.addItems(extent_layer_list)
+
+        # Turn on the save layer button
         self.buttonBox.accepted.connect(self.save_layer)
 
     # TODO Probably move this somewhere and use it all over the place in layer naming
@@ -56,10 +65,12 @@ class ProjectLayerDlg(QDialog, DIALOG_CLASS):
         self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(True)
 
     def save_layer(self):
+        """Saves the imported layer and clips it by a specified extent"""
         # Get form values
         new_display_name = self.lineEdit_display_name.text()
         new_feature_name = self.lineEdit_feature_name.text()
         new_description = self.plainTextEdit_layer_description.toPlainText()
+        clip_extent_name = self.comboBox_clip_extent.currentText()
 
         # Create an instance of the ProjectExtent class
         new_vector_instance = ProjectVectorLayer(new_display_name, new_feature_name, new_description)
@@ -77,7 +88,22 @@ class ProjectLayerDlg(QDialog, DIALOG_CLASS):
             QMessageBox.information(None, "No Layer to Import", "Please select a valid layer to import")
             return
 
-            # Create the geopackage and set write options
+        # CLIP THE FEATURE
+        # get the extent feature by value
+        clip_extent_feature_name = ''
+        for key, value in self.qris_project.project_extents.items():
+            if value.display_name == clip_extent_name:
+                clip_extent_feature_name = key
+
+        extent_instance = self.qris_project.project_extents[clip_extent_feature_name]
+        extent_path = extent_instance.full_path(self.qris_project.project_path)
+        # reference the extent layer
+        extent_layer = QgsVectorLayer(extent_path, 'clip_layer', 'ogr')
+
+        # clip the layer
+        new_layer_clipped = self.clip_layer(new_vector_layer, extent_layer)
+
+        # Create the geopackage and set write options
         if not os.path.exists(vector_directory_path):
             os.makedirs(vector_directory_path)
         options = QgsVectorFileWriter.SaveVectorOptions()
@@ -85,10 +111,20 @@ class ProjectLayerDlg(QDialog, DIALOG_CLASS):
         options.driverName = 'GPKG'
         if os.path.exists(vector_geopackage_path):
             options.actionOnExistingFile = QgsVectorFileWriter.CreateOrOverwriteLayer
-        QgsVectorFileWriter.writeAsVectorFormat(new_vector_layer, vector_geopackage_path, options)
+        QgsVectorFileWriter.writeAsVectorFormat(new_layer_clipped, vector_geopackage_path, options)
 
         # Add the new feature to the project vector dictionary
         self.qris_project.project_vector_layers[new_feature_name] = new_vector_instance
         # TODO double check on whether you need to call the write_project_xml here? or if it gets tripped somewhere else?
         self.qris_project.write_project_xml()
         self.dataChange.emit(self.qris_project, new_display_name)
+
+    def clip_layer(self, input_layer, mask_layer):
+        """Applies a clip to the imported layer based on the selected project extent layer"""
+        p_clipped_layer = processing.run("native:clip",
+                                         {'INPUT': input_layer,
+                                          'OVERLAY': mask_layer,
+                                          'OUTPUT': 'TEMPORARY_OUTPUT'})
+
+        clipped_layer = p_clipped_layer['OUTPUT']
+        return clipped_layer
