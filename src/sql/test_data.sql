8a1b8faa0fd9c773b1b65500948cf99fc943f7af
Nick Weber <webernick@icloud.com>
2022-05-17 16:03:58 -0700

    Begins assessments to maps

--- src/sql/test_data.sql
+++ src/sql/test_data.sql
@@ -1,10 +1,17 @@
 -- a test project with a jam and dam survey
 
-
 INSERT INTO projects (fid, name) VALUES (1, 'Asotin IMW');
+INSERT INTO projects (fid, name) VALUES (2, 'Whychus Creek');
+
 
 INSERT INTO assessments (fid, project_id, start_date) VALUES (1, 1,'2021/10/31');
 INSERT INTO assessments (fid, project_id, start_date) VALUES (2, 1,'2020/6/23');
+INSERT INTO assessments (fid, project_id, start_date) VALUES (3, 2,'2020/6/23');
+
 
 INSERT INTO assessment_methods (fid, assessment_id, method_id, platform_id) VALUES (1, 1, 3, 1);
 INSERT INTO assessment_methods (fid, assessment_id, method_id, platform_id) VALUES (2, 1, 1, 1);
+INSERT INTO assessment_methods (fid, assessment_id, method_id, platform_id) VALUES (3, 1, 1, 1);
+INSERT INTO assessment_methods (fid, assessment_id, method_id, platform_id) VALUES (4, 3, 1, 1);
+INSERT INTO assessment_methods (fid, assessment_id, method_id, platform_id) VALUES (5, 3, 3, 1);
+
