5a2710758b833e7213ef590958971222056b8c03
Philip <philip@northarrowresearch.com>
2022-05-26 12:09:21 -0700

    mask import working

--- src/model/mask.py
+++ src/model/mask.py
@@ -6,7 +6,7 @@ MASK_MACHINE_CODE = 'Mask'
 
 class Mask(DBItem):
 
-    def __init__(self, id, name, mask_type, description):
+    def __init__(self, id: int, name: str, mask_type, description: str):
         super().__init__(id, name)
         self.description = description
         self.mask_type = mask_type
@@ -21,3 +21,35 @@ def load_masks(curs: sqlite3.Cursor, mask_types: dict) -> dict:
         mask_types[row['mask_type_id']],
         row['description']
     ) for row in curs.fetchall()}
+
+
+def insert_mask(db_path: str, name: str, mask_type: DBItem, description: str) -> Mask:
+
+    mask = None
+    with sqlite3.connect(db_path) as conn:
+        try:
+            curs = conn.cursor()
+            curs.execute('INSERT INTO masks (name, mask_type_id, description) VALUES (?, ?, ?)', [name, mask_type.id, description])
+            id = curs.lastrowid
+            mask = Mask(id, name, mask_type, description)
+            conn.commit()
+
+        except Exception as ex:
+            mask = None
+            conn.rollback()
+            raise ex
+
+    return mask
+
+
+def delete_mask(db_path: str, mask_id: int):
+
+    with sqlite3.connect(db_path) as conn:
+        try:
+            curs = conn.cursor()
+            curs.execute('DELETE FROM masks WHERE mask_id = ?', [mask_id])
+            conn.commit()
+
+        except Exception as ex:
+            conn.rollback()
+            raise ex
--- src/processing_provider/feature_class_functions.py
+++ src/processing_provider/feature_class_functions.py
@@ -12,11 +12,12 @@ def check_geometry_type(path) -> int:
 
 def import_mask(source_path: str, dest_path: str, mask_id: int) -> None:
 
+    ogr.UseExceptions()
+
     src_path, src_layer_name = source_path.split('|layername=')
     src_dataset = ogr.Open(src_path)
     src_layer = src_dataset.GetLayer(src_layer_name if src_layer_name is not None else 0)
     src_srs = src_layer.GetSpatialRef()
-    # mask_id_field_index = src_layer
 
     gpkg_driver = ogr.GetDriverByName('GPKG')
     dst_dataset = gpkg_driver.Open(dest_path, 1)
@@ -36,11 +37,9 @@ def import_mask(source_path: str, dest_path: str, mask_id: int) -> None:
         dst_feature = ogr.Feature(dst_layer_def)
         dst_feature.SetGeometry(geom)
         dst_feature.SetField('mask_id', mask_id)
-        dst_feature.SetField('fid', 1)
         err = dst_layer.CreateFeature(dst_feature)
         print(err)
         dst_feature = None
 
-    # dst_feature = None
     src_dataset = None
     dst_dataset = None
