750a56deaccf4d2d4bc4f4834bd49635a9e0c081
Philip <philip@northarrowresearch.com>
2022-07-25 14:31:46 -0700

    fixing SQL schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -611,12 +611,12 @@ ALTER TABLE brat_cis ADD COLUMN observer_name lkp_brat_combined_cis;
 ALTER TABLE brat_cis ADD COLUMN reach_id TEXT;
 ALTER TABLE brat_cis ADD COLUMN observation_date DATE;
 ALTER TABLE brat_cis ADD COLUMN reach_length FLOAT;
-ALTER TABLE brat_cis ADD COLUMN base_streampower_id INT NOT NULL REFERENCES lkp_brat_base_streampower(id);
-ALTER TABLE brat_cis ADD COLUMN high_streampower_id INT NOT NULL REFERENCES lkp_brat_high_streampower(id);
-ALTER TABLE brat_cis ADD COLUMN streamside_veg_id INT NOT NULL REFERENCES lkp_brat_vegetation_types(id);
-ALTER TABLE brat_cis ADD COLUMN riparian_veg_id INT NOT NULL REFERENCES lkp_brat_vegetation_types(id);
-ALTER TABLE brat_cis ADD COLUMN veg_density_id INT NOT NULL REFERENCES lkp_brat_dam_density(id);
-ALTER TABLE brat_cis ADD COLUMN combined_density_id INT NOT NULL REFERENCES lkp_brat_dam_density(id);
+ALTER TABLE brat_cis ADD COLUMN base_streampower_id INT REFERENCES lkp_brat_base_streampower(id);
+ALTER TABLE brat_cis ADD COLUMN high_streampower_id INT REFERENCES lkp_brat_high_streampower(id);
+ALTER TABLE brat_cis ADD COLUMN streamside_veg_id INT REFERENCES lkp_brat_vegetation_types(id);
+ALTER TABLE brat_cis ADD COLUMN riparian_veg_id INT REFERENCES lkp_brat_vegetation_types(id);
+ALTER TABLE brat_cis ADD COLUMN veg_density_id INT REFERENCES lkp_brat_dam_density(id);
+ALTER TABLE brat_cis ADD COLUMN combined_density_id INT REFERENCES lkp_brat_dam_density(id);
 ALTER TABLE brat_cis ADD COLUMN notes TEXT;
 
 ----------------------------------------------------------------------------------------------------------------
