e3e462ce961694386d9716a8bbbb22a9a6e7e928
Philip <philip@northarrowresearch.com>
2022-08-23 15:00:09 -0700

    pour points manual form (again)

--- src/view/frm_pour_point.py
+++ src/view/frm_pour_point.py
@@ -1,22 +1,16 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from qgis.PyQt.QtWidgets import QMessageBox, QDialog
-from ..model.project import Project
-from .ui.pour_point import Ui_PoutPoint
 
 
-class FrmPourPoint(QDialog):  # , Ui_PoutPoint):
+class FrmPourPoint(QtWidgets.QDialog):
 
     def __init__(self, parent, latitude, longitude):
         super().__init__(parent)
-        self.setupUi(self)
+        self.setupUi()
 
         self.setWindowTitle('Create New Pour Point with Catchment')
 
-        # self.txtLatitude.setText(str(latitude))
-        # self.txtLongitude.setText(str(longitude))
-
-        # self.txtLatitude.setReadOnly(True)
-        # self.txtLongitude.setReadOnly(True)
+        self.txtLatitude.setText(str(latitude))
+        self.txtLongitude.setText(str(longitude))
 
     def accept(self):
 
@@ -27,49 +21,80 @@ class FrmPourPoint(QDialog):  # , Ui_PoutPoint):
 
         super().accept()
 
-    def setupUi2(self, frm):
+    def setupUi(self):
         # self.setObjectName("PoutPoint")
         self.resize(640, 480)
 
-        self.verticalLayout = QtWidgets.QVBoxLayout(frm)
+        self.vert = QtWidgets.QVBoxLayout()
+        self.setLayout(self.vert)
 
-        self.gridla = QtWidgets.QGridLayout(frm)
+        self.grid = QtWidgets.QGridLayout()
+        self.vert.addLayout(self.grid)
 
-        self.gridLayout = QtWidgets.QGridLayout()
-
-        self.lblName = QtWidgets.QLabel(self)
+        self.lblName = QtWidgets.QLabel()
+        self.lblName.setText('Name')
         self.grid.addWidget(self.lblName, 0, 0, 1, 1)
 
-        # self.buttonBox = QtWidgets.QDialogButtonBox(PoutPoint)
-        # self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        # self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
-        # self.buttonBox.setObjectName("buttonBox")
-        # self.gridLayout.addWidget(self.buttonBox, 6, 1, 1, 1)
-        # self.chkBasinChars = QtWidgets.QCheckBox(PoutPoint)
-        # self.chkBasinChars.setObjectName("chkBasinChars")
-        # self.gridLayout.addWidget(self.chkBasinChars, 3, 1, 1, 1)
-        # self.txtDescription = QtWidgets.QTextEdit(PoutPoint)
-        # self.txtDescription.setObjectName("txtDescription")
-        # self.gridLayout.addWidget(self.txtDescription, 5, 1, 1, 1)
-        # self.lb = QtWidgets.QLabel(PoutPoint)
-        # self.lb.setObjectName("lb")
-        # self.gridLayout.addWidget(self.lb, 1, 0, 1, 1)
-        # self.txtLongitude = QtWidgets.QLineEdit(PoutPoint)
-        # self.txtLongitude.setObjectName("txtLongitude")
-        # self.gridLayout.addWidget(self.txtLongitude, 2, 1, 1, 1)
-        # self.txtLatitude = QtWidgets.QLineEdit(PoutPoint)
-        # self.txtLatitude.setObjectName("txtLatitude")
-        # self.gridLayout.addWidget(self.txtLatitude, 1, 1, 1, 1)
-        # self.Description = QtWidgets.QLabel(PoutPoint)
-        # self.Description.setObjectName("Description")
-        # self.gridLayout.addWidget(self.Description, 5, 0, 1, 1)
-        # self.label_3 = QtWidgets.QLabel(PoutPoint)
-        # self.label_3.setObjectName("label_3")
-        # self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
-        # self.txtName = QtWidgets.QLineEdit(PoutPoint)
-        # self.txtName.setObjectName("txtName")
-        # self.gridLayout.addWidget(self.txtName, 0, 1, 1, 1)
-        # self.chkFlowStats = QtWidgets.QCheckBox(PoutPoint)
-        # self.chkFlowStats.setObjectName("chkFlowStats")
-        # self.gridLayout.addWidget(self.chkFlowStats, 4, 1, 1, 1)
-        # self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
+        self.txtName = QtWidgets.QLineEdit()
+        self.txtName.setMaxLength(255)
+        self.grid.addWidget(self.txtName, 0, 1, 1, 1)
+
+        self.horiz = QtWidgets.QHBoxLayout()
+        self.vert.addLayout(self.horiz)
+
+        self.lblLatitude = QtWidgets.QLabel()
+        self.lblLatitude.setText('Latitude')
+        self.horiz.addWidget(self.lblLatitude)
+
+        self.txtLatitude = QtWidgets.QLineEdit()
+        self.txtLatitude.setReadOnly(True)
+        self.horiz.addWidget(self.txtLatitude)
+
+        self.lblLongitude = QtWidgets.QLabel()
+        self.lblLongitude.setText('Longitude')
+        self.horiz.addWidget(self.lblLongitude)
+
+        self.txtLongitude = QtWidgets.QLineEdit()
+        self.txtLongitude.setReadOnly(True)
+        self.horiz.addWidget(self.txtLongitude)
+
+        self.chkDelineate = QtWidgets.QCheckBox()
+        self.chkDelineate.setText('Delineate Watershed Catchment')
+        self.chkDelineate.setEnabled(False)
+        self.chkDelineate.setChecked(True)
+        self.vert.addWidget(self.chkDelineate)
+
+        self.chkBasin = QtWidgets.QCheckBox()
+        self.chkBasin.setText('Calculate Basin Characteristics (additional 60 secs)')
+        self.vert.addWidget(self.chkBasin)
+
+        self.chkFlowStats = QtWidgets.QCheckBox()
+        self.chkFlowStats.setText('Calculate Flow Statistics (additional 60 secs)')
+        self.vert.addWidget(self.chkFlowStats)
+
+        self.tabWidget = QtWidgets.QTabWidget()
+        self.vert.addWidget(self.tabWidget)
+
+        self.txtDescription = QtWidgets.QPlainTextEdit()
+        self.tabWidget.addTab(self.txtDescription, 'Description')
+
+        self.tabBasin = QtWidgets.QTableWidget()
+        self.tabWidget.addTab(self.tabBasin, 'Basin Characteristics')
+
+        self.tabFlow = QtWidgets.QTableWidget()
+        self.tabWidget.addTab(self.tabFlow, 'Flow Statistics')
+
+        self.buttonBox = QtWidgets.QDialogButtonBox()
+        self.vert.addWidget(self.buttonBox)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+
+if __name__ == "__main__":
+
+    app = QtWidgets.QApplication([])
+    window = FrmPourPoint(None, 123, 123)
+    window.show()
+    app.exec_()
