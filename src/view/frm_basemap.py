ea7a8e192945493bffc98b1ac884f091260f54c2
Philip <philip@northarrowresearch.com>
2022-08-24 20:54:24 -0700

    manual basemap form

--- src/view/frm_basemap.py
+++ src/view/frm_basemap.py
@@ -1,10 +1,6 @@
 import os
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-from qgis.PyQt.QtWidgets import QDialog, QDialogButtonBox, QFileDialog, QDialogButtonBox, QMessageBox
-from qgis.PyQt.QtCore import pyqtSignal, QVariant, QUrl, QRect, Qt
-from qgis.PyQt.QtGui import QIcon, QDesktopServices, QStandardItemModel, QStandardItem
-
-from .ui.basis import Ui_Basis
 from ..model.basemap import BASEMAP_PARENT_FOLDER, Basemap, insert_basemap
 from ..model.db_item import DBItemModel, DBItem
 from ..model.project import Project
@@ -13,7 +9,7 @@ from ..model.mask import AOI_MASK_TYPE_ID
 from ..gp.feature_class_functions import copy_raster_to_project
 
 
-class FrmBasemap(QDialog, Ui_Basis):
+class FrmBasemap(QtWidgets.QDialog):
 
     def __init__(self, parent, qris_project: Project, import_source_path: str, basemap: Basemap = None):
 
@@ -21,12 +17,8 @@ class FrmBasemap(QDialog, Ui_Basis):
         self.basemap = basemap
 
         super(FrmBasemap, self).__init__(parent)
-        self.setupUi(self)
+        self.setupUi()
         self.setWindowTitle('Create New Basemap' if self.basemap is None else 'Edit Basemap Properties')
-        self.buttonBox.accepted.connect(super(FrmBasemap, self).accept)
-        self.buttonBox.rejected.connect(super(FrmBasemap, self).reject)
-
-        self.gridLayout.setGeometry(QRect(0, 0, self.width(), self.height()))
 
         if basemap is None:
             self.txtName.textChanged.connect(self.on_name_changed)
@@ -51,14 +43,14 @@ class FrmBasemap(QDialog, Ui_Basis):
             self.txtProjectPath.setText(qris_project.get_absolute_path(basemap.path))
 
             self.chkAddToMap.setVisible(False)
-            self.chkAddToMap.setCheckState(Qt.Unchecked)
+            self.chkAddToMap.setCheckState(QtCore.Qt.Unchecked)
 
         self.txtName.selectAll()
 
     def accept(self):
 
         if len(self.txtName.text()) < 1:
-            QMessageBox.warning(self, 'Missing Basis Name', 'You must provide a basis name to continue.')
+            QtWidgets.QtWidgets.QMessageBox.warning(self, 'Missing Basis Name', 'You must provide a basis name to continue.')
             self.txtName.setFocus()
             return()
 
@@ -67,10 +59,10 @@ class FrmBasemap(QDialog, Ui_Basis):
                 self.basemap.update(self.qris_project.project_file, self.txtName.text(), self.txtDescription.toPlainText())
             except Exception as ex:
                 if 'unique' in str(ex).lower():
-                    QMessageBox.warning(self, 'Duplicate Name', "A basemap with the name '{}' already exists. Please choose a unique name.".format(self.txtName.text()))
+                    QtWidgets.QMessageBox.warning(self, 'Duplicate Name', "A basemap with the name '{}' already exists. Please choose a unique name.".format(self.txtName.text()))
                     self.txtName.setFocus()
                 else:
-                    QMessageBox.warning(self, 'Error Saving Basemap', str(ex))
+                    QtWidgets.QMessageBox.warning(self, 'Error Saving Basemap', str(ex))
                 return
         else:
             try:
@@ -78,14 +70,14 @@ class FrmBasemap(QDialog, Ui_Basis):
                 self.qris_project.basemaps[self.basemap.id] = self.basemap
             except Exception as ex:
                 if 'unique' in str(ex).lower():
-                    QMessageBox.warning(self, 'Duplicate Name', "A basemap with the name '{}' already exists. Please choose a unique name.".format(self.txtName.text()))
+                    QtWidgets.QMessageBox.warning(self, 'Duplicate Name', "A basemap with the name '{}' already exists. Please choose a unique name.".format(self.txtName.text()))
                     self.txtName.setFocus()
                 else:
-                    QMessageBox.warning(self, 'Error Saving Basemap', str(ex))
+                    QtWidgets.QMessageBox.warning(self, 'Error Saving Basemap', str(ex))
                 return
 
             try:
-                mask = self.cboMask.currentData(Qt.UserRole)
+                mask = self.cboMask.currentData(QtCore.Qt.UserRole)
                 mask_tuple = (self.qris_project.project_file, mask.id) if mask.id > 0 else None
 
                 project_path = self.qris_project.get_absolute_path(self.txtProjectPath.text())
@@ -100,7 +92,7 @@ class FrmBasemap(QDialog, Ui_Basis):
                 except Exception as ex:
                     print('Error attempting to delete basemap after the importing raster failed.')
                 self.basemap = None
-                QMessageBox.warning(self, 'Error Importing Basemap', str(ex))
+                QtWidgets.QMessageBox.warning(self, 'Error Importing Basemap', str(ex))
                 return
 
         super(FrmBasemap, self).accept()
@@ -114,3 +106,70 @@ class FrmBasemap(QDialog, Ui_Basis):
             self.txtProjectPath.setText(os.path.join(BASEMAP_PARENT_FOLDER, self.qris_project.get_safe_file_name(clean_name, ext)))
         else:
             self.txtProjectPath.setText('')
+
+    def setupUi(self):
+
+        self.vert = QtWidgets.QVBoxLayout()
+        self.setLayout(self.vert)
+
+        self.grid = QtWidgets.QGridLayout()
+        self.vert.addLayout(self.grid)
+
+        self.lblName = QtWidgets.QLabel()
+        self.lblName.setText('Name')
+        self.grid.addWidget(self.lblName, 0, 0, 1, 1)
+
+        self.txtName = QtWidgets.QLineEdit()
+        self.txtName.setMaxLength(255)
+        self.grid.addWidget(self.txtName, 0, 1, 1, 1)
+
+        self.lblSourcePath = QtWidgets.QLabel()
+        self.lblSourcePath.setText('Source path')
+        self.grid.addWidget(self.lblSourcePath, 1, 0, 1, 1)
+
+        self.txtSourcePath = QtWidgets.QLineEdit()
+        self.txtSourcePath.setReadOnly(True)
+        self.grid.addWidget(self.txtSourcePath, 1, 1, 1, 1)
+
+        self.lblProjectPath = QtWidgets.QLabel()
+        self.lblProjectPath.setText('Project Path')
+        self.grid.addWidget(self.lblProjectPath, 2, 1, 1, 1)
+
+        self.txtProjectPath = QtWidgets.QLineEdit()
+        self.grid.addWidget(self.txtProjectPath, 2, 1, 1, 1)
+
+        self.lblMask = QtWidgets.QLabel()
+        self.lblMask.setText('Clip to mask')
+        self.grid.addWidget(self.lblMask, 3, 0, 1, 1)
+
+        self.cboMask = QtWidgets.QComboBox()
+        self.grid.addWidget(self.cboMask, 3, 1, 1, 1)
+
+        self.lblDescription = QtWidgets.QLabel()
+        self.lblDescription.setText('Description')
+        self.grid.addWidget(self.lblDescription, 4, 0, 1, 1)
+
+        self.txtDescription = QtWidgets.QPlainTextEdit()
+        self.grid.addWidget(self.txtDescription, 4, 1, 1, 1)
+
+        self.chkAddToMap = QtWidgets.QCheckBox()
+        self.chkAddToMap.setText('Add to Map')
+        self.chkAddToMap.setChecked(True)
+        self.grid.addWidget(self.chkAddToMap, 5, 1, 1, 1)
+
+        self.horiz = QtWidgets.QHBoxLayout()
+        self.vert.addLayout(self.horiz)
+
+        self.cmdHelp = QtWidgets.QPushButton()
+        self.cmdHelp.setText('Help')
+        self.horiz.addWidget(self.cmdHelp)
+
+        self.spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horiz.addItem(self.spacerItem)
+
+        self.buttonBox = QtWidgets.QDialogButtonBox()
+        self.horiz.addWidget(self.buttonBox)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
