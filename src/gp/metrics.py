9875c9f565845037c47689ada720a94bfbef78f1
Philip <philip@northarrowresearch.com>
2022-09-28 15:55:43 -0700

    first version of stats working

---
+++ src/gp/metrics.py
@@ -0,0 +1,208 @@
+import os
+import json
+import numpy as np
+import math
+import rasterio
+from rasterio.mask import mask
+import osgeo
+from osgeo import ogr, gdal, osr
+from shapely.wkb import load, loads as wkbload, dumps as wkbdumps
+from osgeo import gdal
+from shapely.geometry import mapping
+
+
+class Metrics:
+
+    def __init__(self, project_file: str, mask_id: int, layers: list):
+
+        self.config = {'vector': {}, 'raster': {}}
+        self.project_file = project_file
+        self.mask_id = mask_id
+        self.layers = layers
+        self.metrics = {}
+        self.polygons, self.utm_epsg = self.load_polygons(project_file, mask_id)
+        # print(json.dumps(mapping(self.polygons[2]['geometry'])))
+
+    def load_polygons(self, project_file: str, mask_id: int) -> dict:
+
+        driver = ogr.GetDriverByName("GPKG")
+        ds = driver.Open(project_file)
+        layer = ds.GetLayerByName('mask_features')
+        layer.SetAttributeFilter(f'mask_id = {mask_id}')
+        src_srs = layer.GetSpatialRef()
+
+        # Target transform to most appropriate UTM zone
+        utm_transform = None
+        epsg = None
+
+        polygons = {}
+        for feature in layer:
+            geom = feature.GetGeometryRef()
+
+            if utm_transform is None:
+                temp_temp = geom.Clone()
+
+                # Temporarily transform to WGS84 to determine best UTM zone
+                wgs_srs = osr.SpatialReference()
+                wgs_srs.ImportFromEPSG(4326)
+                wgs_transform = osr.CoordinateTransformation(src_srs, wgs_srs)
+                temp_temp.Transform(wgs_transform)
+
+                epsg = self.get_utm_zone_epsg(geom.Centroid().GetX())
+                utm_srs = osr.SpatialReference()
+                utm_srs.ImportFromEPSG(epsg)
+                utm_transform = osr.CoordinateTransformation(src_srs, utm_srs)
+
+            geom.Transform(utm_transform)
+
+            if geom.IsMeasured() > 0 or geom.Is3D() > 0:
+                geom.FlattenTo2D()
+            polygons[feature.GetFID()] = {
+                'geometry': wkbload(bytes(geom.ExportToWkb())),
+                'display_label': feature.GetField('display_label')
+            }
+
+        layer = None
+        ds = None
+
+        if len(polygons) < 1:
+            raise Exception('Mask Feature Class is empty. No polygons loaded.')
+
+        return polygons, epsg
+
+    def get_utm_zone_epsg(self, longitude: float) -> int:
+        """Really crude EPSG lookup method
+
+        Args:
+            longitude (float): [description]
+
+        Returns:
+            int: [description]
+        """
+        zone_number = math.floor((180.0 + longitude) / 6.0)
+        epsg = 26901 + zone_number
+        return epsg
+
+    def run(self) -> dict:
+
+        metrics = {}
+        for layer in self.layers:
+            if layer['type'] == 'vector':
+                metrics[layer['name']] = self.process_vector(layer)
+            else:
+                metrics[layer['name']] = self.process_raster(layer)
+
+        return metrics
+
+    def process_vector(self, layer_def):
+
+        ds = ogr.Open(layer_def['url'])
+        layer = ds.GetLayerByName(layer_def['name']) if ds.GetLayerCount() > 1 else ds.GetLayer(0)
+        src_srs = layer.GetSpatialRef()
+
+        dst_srs = osr.SpatialReference()
+        dst_srs.ImportFromEPSG(self.utm_epsg)
+        transform_src_to_utm = osr.CoordinateTransformation(src_srs, dst_srs)
+
+        # Used for transforming polygons onto the layer SRS for spatial filter
+        transform_utm_to_src = osr.CoordinateTransformation(dst_srs, src_srs)
+
+        metrics = {}
+        for polygon_id, polygon_data in self.polygons.items():
+            polygon = polygon_data['geometry']
+            polygon_geom = ogr.CreateGeometryFromWkb(polygon.wkb)
+            polygon_geom.Transform(transform_utm_to_src)
+
+            polygon_metrics = {'count': 0}
+            layer.SetSpatialFilter(polygon_geom)
+            for feature in layer:
+                geom = feature.GetGeometryRef()
+                geom.Transform(transform_src_to_utm)
+
+                if geom.IsMeasured() > 0 or geom.Is3D() > 0:
+                    geom.FlattenTo2D()
+
+                shape = wkbload(bytes(geom.ExportToWkb()))
+                if polygon.intersects(shape):
+                    inter = polygon.intersection(shape)
+                    polygon_metrics['count'] += 1
+
+                    if inter.geom_type == 'LineString' or inter.geom_type == 'MultiLineString':
+                        polygon_metrics['area'] += inter.length
+                    elif inter.geom_type == 'Polygon' or inter.geom_type == 'MultiPolygon':
+                        polygon_metrics['length'] += inter.area
+
+            metrics[polygon_id] = polygon_metrics
+
+        return metrics
+
+    def process_raster(self, layer_def):
+
+        raster = gdal.Open(layer_def['url'])
+        # src_srs = raster.GetProjection()
+        src_srs = osr.SpatialReference()
+        src_srs.ImportFromWkt(raster.GetProjection())
+
+        # Handle GDAL axis mapping issues
+        # https://github.com/OSGeo/gdal/issues/1546
+        if int(osgeo.__version__[0]) >= 3:
+            # GDAL 3 changes axis order: https://github.com/OSGeo/gdal/issues/1546
+            src_srs.SetAxisMappingStrategy(osgeo.osr.OAMS_TRADITIONAL_GIS_ORDER)
+
+        raster = None
+
+        # Used for transforming polygons onto the raster  SRS
+        dst_srs = osr.SpatialReference()
+        dst_srs.ImportFromEPSG(self.utm_epsg)
+        transform_utm_to_src = osr.CoordinateTransformation(dst_srs, src_srs)
+
+        results = {}
+        with rasterio.open(layer_def['url']) as src:
+            for polygon_id, polygon_data in self.polygons.items():
+
+                mean = maximum = minimum = count = rsum = None
+                try:
+                    polygon = polygon_data['geometry']
+                    polygon_geom = ogr.CreateGeometryFromWkb(polygon.wkb)
+                    polygon_geom.Transform(transform_utm_to_src)
+                    shape = wkbload(bytes(polygon_geom.ExportToWkb()))
+                    # print(json.dumps(mapping(shape)))
+
+                    # retrieve an array for the cells under the polygon
+                    raw_raster, _out_transform = mask(src, [shape], crop=True)
+                    mask_raster = np.ma.masked_values(raw_raster, src.nodata)
+                    # print(mask_raster)
+
+                    if not mask_raster.mask.all():
+                        mean = float(mask_raster.mean())
+                        maximum = float(mask_raster.max())
+                        minimum = float(mask_raster.min())
+                        count = int(mask_raster.count())
+                        rsum = float(mask_raster.sum())
+                except Exception as ex:
+                    if 'input shapes do not overlap raster' not in str(ex).lower():
+                        raise Exception(f"Error processing polygon {polygon_id} in raster {layer_def['name']}", ex)
+
+                results[polygon_id] = {'Mean': mean, 'Maximum': maximum, 'Minimum': minimum, 'Count': count, 'Sum': rsum}
+
+        return results
+
+    # def categorical_raster_metrics(self, dataset_name, raster_path):
+
+    #     cell_area = get_raster_cell_area(raster_path)
+
+    #     cats = {key: {} for key in self.polygons.keys()}
+    #     with rasterio.open(raster_path) as src:
+
+    #         for poly_id, polygon in self.polygons.items():
+
+    #             raw_raster, _out_transform = mask(src, [polygon], crop=True)
+    #             # mask_raster = np.ma.masked_values(raw_raster, src.nodata)
+
+    #             for val in np.unique(raw_raster):
+    #                 if val != src.nodata:
+    #                     cats[poly_id][str(val)] = {'area': np.count_nonzero(raw_raster == val) * cell_area,
+    #                                                'count': np.count_nonzero(raw_raster == val)}
+
+    #     for key in cats.keys():
+    #         self.metrics['project']['huc12'][key]['metrics']['raster']['categorical'].append({dataset_name: {'cellSize': np.sqrt(cell_area), 'categories': cats[key]}})
---
+++ src/gp/metrics_task.py
@@ -0,0 +1,91 @@
+import os
+import json
+import datetime
+from qgis.core import QgsTask, QgsMessageLog, Qgis, QgsProject, QgsVectorLayer, QgsRasterLayer
+from qgis.PyQt.QtCore import pyqtSignal
+
+from ..model.project import Project
+from ..model.mask import Mask
+from .metrics import Metrics
+from ..model.basemap import SCRATCH_PARENT_FOLDER
+import webbrowser
+
+MESSAGE_CATEGORY = 'QRiS_CopyFeatureClassTask'
+
+
+class MetricsTask(QgsTask):
+    """
+    https://docs.qgis.org/3.22/en/docs/pyqgis_developer_cookbook/tasks.html
+    """
+
+    # Signal to notify when done and return the PourPoint and whether it should be added to the map
+    on_complete = pyqtSignal(bool)
+
+    def __init__(self, project: Project, mask: Mask):
+        super().__init__(f'Copy Raster Task', QgsTask.CanCancel)
+
+        self.map_layers = []
+        for layer in QgsProject.instance().mapLayers().values():
+            if QgsProject.instance().layerTreeRoot().findLayer(layer.id()).itemVisibilityChecked():
+                layer_def = {'name:': layer.name, 'url': layer.dataProvider().dataSourceUri()}
+                if isinstance(layer, QgsRasterLayer):
+                    layer_def['type'] = 'raster'
+                    self.map_layers.append(layer_def)
+                elif isinstance(layer, QgsVectorLayer):
+                    layer_def['type'] = 'vector'
+                    self.map_layers.append(layer_def)
+
+        self.project = project
+        self.mask = mask
+        self.config = {}
+
+        self.metrics = Metrics(project.project_file, mask.id, self.map_layers)
+
+    def run(self):
+        """
+        Originally intended to use this VectorTranslate method
+        https://gdal.org/development/rfc/rfc59.1_utilities_as_a_library.html#swig-bindings-python-java-c-perl-changes
+
+        But ended up using this ogr method
+        https://subscription.packtpub.com/book/application-development/9781787124837/3/ch03lvl1sec58/exporting-a-layer-to-the-geopackage-format
+        """
+        try:
+            self.data = self.metrics.run()
+
+        except Exception as ex:
+            self.exception = ex
+            return False
+
+        return True
+
+    def finished(self, result: bool):
+        """
+        This function is automatically called when the task has completed (successfully or not).
+        You implement finished() to do whatever follow-up stuff should happen after the task is complete.
+        finished is always called from the main thread, so it's safe to do GUI operations and raise Python exceptions here.
+        result is the return value from self.run.
+        """
+
+        if result:
+            QgsMessageLog.logMessage('Metrics Complete', MESSAGE_CATEGORY, Qgis.Success)
+
+            base_name = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            output_json = os.path.join(os.path.dirname(self.project.project_file), SCRATCH_PARENT_FOLDER, f'geospatial_metric_summary_{base_name}.json')
+            with open(output_json, 'w') as f:
+                json.dump(self.data, f, indent=4)
+            webbrowser.open('file://' + output_json)
+
+        else:
+            if self.exception is None:
+                QgsMessageLog.logMessage(
+                    'Geospatial Metrics unsuccessful but without exception (probably the task was canceled by the user)', MESSAGE_CATEGORY, Qgis.Warning)
+            else:
+                QgsMessageLog.logMessage(f'Geospatial metrics exception: {self.exception}', MESSAGE_CATEGORY, Qgis.Critical)
+                raise self.exception
+
+        self.copy_complete.emit(result)
+
+    def cancel(self):
+        QgsMessageLog.logMessage(
+            'Geospatial Metrics was canceled'.format(name=self.description()), MESSAGE_CATEGORY, Qgis.Info)
+        super().cancel()
