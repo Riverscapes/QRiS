3b97bacd4b08d688f7e4b779c48f10ebfffcef68
Philip <philip@northarrowresearch.com>
2022-09-29 17:10:40 -0700

    start of units

---
+++ src/view/frm_options.py
@@ -0,0 +1,67 @@
+
+
+import os
+from PyQt5 import QtCore, QtGui, QtWidgets
+from qgis.core import QgsVectorLayer
+
+from ..QRiS.units import get_areas, get_lengths
+
+from ..model.db_item import DBItem, DBItemModel
+from ..model.project import Project
+from ..model.mask import Mask, insert_mask, REGULAR_MASK_TYPE_ID
+
+from ..gp.feature_class_functions import import_mask
+from .utilities import validate_name, add_standard_form_buttons
+from scipy import constants
+
+
+class FrmOptions(QtWidgets.QDialog):
+
+    def __init__(self, parent, project: Project):
+        super().__init__(parent)
+        self.setupUi()
+
+        self.qris_project = project
+
+        for control, data in ((self.cboLength, get_lengths), (self.cboArea, get_areas)):
+            for display_name, unit_key in data().items():
+                # item = QtGui.QStandardItem(display_name)
+                # item.setData(unit_key, QtCore.Qt.UserRole)
+                control.addItem(display_name, unit_key)
+
+        # self.setWindowTitle(f'Geospatial Metrics for {self.mask.name}')
+
+        # self.txtMask.setText(self.mask.name)
+
+        # self.load_tree()
+
+    def accept(self):
+
+        super().accept()
+
+    def setupUi(self):
+
+        self.resize(500, 300)
+        self.setMinimumSize(300, 200)
+
+        self.vert = QtWidgets.QVBoxLayout()
+        self.setLayout(self.vert)
+
+        self.grid = QtWidgets.QGridLayout()
+        self.vert.addLayout(self.grid)
+
+        self.lblLength = QtWidgets.QLabel()
+        self.lblLength.setText('Length')
+        self.grid.addWidget(self.lblLength, 0, 0, 1, 1)
+
+        self.cboLength = QtWidgets.QComboBox()
+        self.grid.addWidget(self.cboLength, 0, 1, 1, 1)
+
+        self.lblArea = QtWidgets.QLabel()
+        self.lblArea.setText('Area')
+        self.grid.addWidget(self.lblArea, 1, 0, 1, 1)
+
+        self.cboArea = QtWidgets.QComboBox()
+        self.grid.addWidget(self.cboArea, 1, 1, 1, 1)
+
+        self.vert.addLayout(add_standard_form_buttons(self, 'options'))
