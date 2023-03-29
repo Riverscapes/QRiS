4faff199bda7d51c31a88073edc200fd3fd9356f
Kelly W <kellymwhitehead@gmail.com>
2023-03-29 09:56:11 -0700

    metrics page

---
+++ docs/Technical_Reference/metrics.py
@@ -0,0 +1,15 @@
+---
+title: QRiS Metric Definitions
+---
+
+# dam and jam count
+
+# Percent Active Floodplain
+
+# Centerline Gradient
+
+# Dam Crest Length
+
+# Valley Bottom Area
+
+# Centerline Sinuosity
