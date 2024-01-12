6b27e446f6713e261a9c7a4fb25f6bbd1128dc16
Kelly W <kellymwhitehead@gmail.com>
2024-01-12 13:28:51 -0800

    fix designs placement in layer tree

--- src/model/event.py
+++ src/model/event.py
@@ -10,6 +10,7 @@ from .event_layer import EventLayer
 from .raster import Raster
 EVENT_MACHINE_CODE = 'Event'
 DESIGN_MACHINE_CODE = 'Design'
+AS_BUILT_MACHINE_CODE = 'AsBuilt'
 
 # Database ID of the design event type. Used to determine which icon to use
 DESIGN_EVENT_TYPE_ID = 2
