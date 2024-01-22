9875947f0a7d614a58a8ab1f36ac40c9dfc7074a
Kelly W <kellymwhitehead@gmail.com>
2024-01-22 11:16:52 -0800

    remove old migrations

--- src/db/migrations/002_raster_change.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE rasters ADD COLUMN is_context BOOLEAN DEFAULT 0;
-
-INSERT INTO lkp_raster_types (id, name) VALUES (6, 'Hillshade');
-
-INSERT INTO method_layers (method_id, layer_id) VALUES (3, 5);
-INSERT INTO method_layers (method_id, layer_id) VALUES (3, 7);
