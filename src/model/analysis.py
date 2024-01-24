9f98c8c298ddab8def7845d71a623872ed9d6b0a
Kelly W <kellymwhitehead@gmail.com>
2024-01-24 12:26:15 -0800

    update for metrics

--- src/model/analysis.py
+++ src/model/analysis.py
@@ -1,5 +1,6 @@
-from email.mime import base
+import json
 import sqlite3
+
 from .db_item import DBItem
 from .raster import Raster
 from .sample_frame import SampleFrame
@@ -10,20 +11,24 @@ ANALYSIS_MACHINE_CODE = 'ANALYSIS'
 
 class Analysis(DBItem):
 
-    def __init__(self, id: int, name: str, description: str, sample_frame: SampleFrame):
+    def __init__(self, id: int, name: str, description: str, sample_frame: SampleFrame, metadata: dict = None):
         super().__init__('analyses', id, name)
         self.description = description
         self.icon = 'analysis'
         self.sample_frame = sample_frame
         self.analysis_metrics = None
+        self.metadata = metadata
+        self.profile = metadata.get('profile', None) if metadata is not None else None # really just the profile id
 
-    def update(self, db_path: str, name: str, description: str, analysis_metrics: dict) -> None:
+    def update(self, db_path: str, name: str, description: str, analysis_metrics: dict, metadata: dict = None) -> None:
 
         description = description if len(description) > 0 else None
+        metadata_str = json.dumps(metadata) if metadata is not None else None
+
         with sqlite3.connect(db_path) as conn:
             try:
                 curs = conn.cursor()
-                curs.execute('UPDATE analyses SET name = ?, description = ? WHERE id = ?', [name, description, self.id])
+                curs.execute('UPDATE analyses SET name = ?, description = ?, metadata = ? WHERE id = ?', [name, description, metadata_str, self.id])
 
                 store_analysis_metrics(curs, self.id, analysis_metrics)
 
@@ -32,6 +37,7 @@ class Analysis(DBItem):
                 self.name = name
                 self.description = description
                 self.analysis_metrics = analysis_metrics
+                self.metadata = metadata
 
             except Exception as ex:
                 conn.rollback()
@@ -45,7 +51,8 @@ def load_analyses(curs: sqlite3.Cursor, sample_frames: dict, metrics: dict) -> d
         row['id'],
         row['name'],
         row['description'],
-        sample_frames[row['mask_id']]
+        sample_frames[row['mask_id']],
+        json.loads(row['metadata']) if row['metadata'] is not None else None
     ) for row in curs.fetchall()}
 
     for analysis_id, analysis in analyses.items():
@@ -55,19 +62,21 @@ def load_analyses(curs: sqlite3.Cursor, sample_frames: dict, metrics: dict) -> d
     return analyses
 
 
-def insert_analysis(db_path: str, name: str, description: str, sample_frame: SampleFrame, analysis_metrics: dict) -> Analysis:
+def insert_analysis(db_path: str, name: str, description: str, sample_frame: SampleFrame, analysis_metrics: dict, metadata: dict=None) -> Analysis:
     """
     active metrics is a dictionary with metric_id keyed to metric_level_id
     """
+
+    metadata_str = json.dumps(metadata) if metadata is not None else None
 
     result = None
     with sqlite3.connect(db_path) as conn:
         try:
             curs = conn.cursor()
-            curs.execute('INSERT INTO analyses (name, description, mask_id) VALUES (?, ?, ?)', [
-                name, description if description is not None and len(description) > 0 else None, sample_frame.id])
+            curs.execute('INSERT INTO analyses (name, description, mask_id, metadata) VALUES (?, ?, ?, ?)', [
+                name, description if description is not None and len(description) > 0 else None, sample_frame.id, metadata_str])
             analysis_id = curs.lastrowid
-            analysis = Analysis(analysis_id, name, description, sample_frame)
+            analysis = Analysis(analysis_id, name, description, sample_frame, metadata=metadata)
 
             store_analysis_metrics(curs, analysis_id, analysis_metrics)
             analysis.analysis_metrics = analysis_metrics
