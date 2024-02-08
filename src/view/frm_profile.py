d22674fc457952cce4c4d088f900330f8504d4d8
Kelly W <kellymwhitehead@gmail.com>
2024-02-08 13:04:40 -0800

    fix import profiles

--- src/view/frm_profile.py
+++ src/view/frm_profile.py
@@ -1,13 +1,12 @@
 import json
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
 from qgis.core import QgsApplication, QgsVectorLayer
 from qgis.utils import Qgis, iface
 
 from ..model.db_item import DBItem, DBItemModel
 from ..model.project import Project
 from ..model.profile import Profile, insert_profile
-from ..model.mask import AOI_MASK_TYPE_ID
 
 from ..gp.feature_class_functions import import_existing, layer_path_parser
 from ..gp.import_temp_layer import ImportTemporaryLayer
@@ -32,11 +31,6 @@ class FrmProfile(QtWidgets.QDialog):
 
         self.setWindowTitle(f'Create New Profile' if self.profile is None else f'Edit Profile Properties')
 
-        # The attribute picker is only visible when creating a new regular mask
-        show_attribute_filter = import_source_path is not None
-        self.lblAttribute.setVisible(show_attribute_filter)
-        self.cboAttribute.setVisible(show_attribute_filter)
-
         show_mask_clip = import_source_path is not None
         self.lblMaskClip.setVisible(show_mask_clip)
         self.cboMaskClip.setVisible(show_mask_clip)
@@ -45,8 +39,7 @@ class FrmProfile(QtWidgets.QDialog):
             if isinstance(import_source_path, QgsVectorLayer):
                 self.layer_name = import_source_path.name()
                 self.layer_id = 'memory'
-                show_attribute_filter = False
-                show_mask_clip = False
+                #  show_mask_clip = False
             else:
                 # find if import_source_path is shapefile, geopackage, or other
                 self.basepath, self.layer_name, self.layer_id = layer_path_parser(import_source_path)
@@ -54,21 +47,18 @@ class FrmProfile(QtWidgets.QDialog):
             self.txtName.setText(self.layer_name)
             self.txtName.selectAll()
 
-            if show_attribute_filter:
-                vector_layer = import_source_path if isinstance(import_source_path, QgsVectorLayer) else QgsVectorLayer(import_source_path)
-                self.attributes = {i: DBItem('None', i, vector_layer.attributeDisplayName(i)) for i in vector_layer.attributeList()}
-                self.attribute_model = DBItemModel(self.attributes)
-                self.cboAttribute.setModel(self.attribute_model)
-                # self.cboAttribute.setModelColumn(1)
             if show_mask_clip:
                 # Masks (filtered to just AOI)
-                self.masks = {id: mask for id, mask in self.qris_project.masks.items() if mask.mask_type.id == AOI_MASK_TYPE_ID}
+                self.aois = {id: aoi for id, aoi in self.qris_project.aois.items()}
                 no_clipping = DBItem('None', 0, 'None - Retain full dataset extent')
-                self.masks[0] = no_clipping
-                self.masks_model = DBItemModel(self.masks)
-                self.cboMaskClip.setModel(self.masks_model)
+                self.aois[0] = no_clipping
+                self.aoi_model = DBItemModel(self.aois)
+                self.cboMaskClip.setModel(self.aoi_model)
                 # Default to no mask clipping
-                self.cboMaskClip.setCurrentIndex(self.masks_model.getItemIndex(no_clipping))
+                self.cboMaskClip.setCurrentIndex(self.aoi_model.getItemIndex(no_clipping))
+            else:
+                self.lblMaskClip.setVisible(False)
+                self.cboMaskClip.setVisible(False)
 
         if self.profile is not None:
             self.txtName.setText(profile.name)
@@ -114,24 +104,28 @@ class FrmProfile(QtWidgets.QDialog):
             if self.import_source_path is not None:
                 try:
                     clip_mask = self.cboMaskClip.currentData(QtCore.Qt.UserRole)
-                    clip_mask_id = None
+                    clip_mask = None if clip_mask.id == 0 else clip_mask
+                    clip_aoi_id = None
                     if clip_mask is not None:
-                        clip_mask_id = clip_mask.id if clip_mask.id > 0 else None
+                        clip_aoi_id = clip_mask.id if clip_mask.id > 0 else None
                     if self.layer_id == 'memory':
-                        task = ImportTemporaryLayer(self.import_source_path, self.qris_project.project_file, 'profile_features', 'profile_id', self.profile.id, mask_clip_id=clip_mask_id, proj_gpkg=self.qris_project.project_file)
+                        task = ImportTemporaryLayer(self.import_source_path, self.qris_project.project_file, 'profile_features', {'profile_id': self.profile.id}, mask_clip_id=clip_aoi_id, proj_gpkg=self.qris_project.project_file)
                         # DEBUG task.run()
                         task.import_complete.connect(self.on_import_complete)
                         QgsApplication.taskManager().addTask(task)
                     else:
-                        attributes = {self.cboAttribute.currentData(QtCore.Qt.UserRole).name: 'display_label'} if self.cboAttribute.isVisible() else {}
-                        import_existing(self.import_source_path, self.qris_project.project_file, 'profile_features', self.profile.id, 'profile_id', attributes, clip_mask_id)
+                        import_existing(self.import_source_path, self.qris_project.project_file, 'profile_features', self.profile.id, 'profile_id', clip_mask_id=clip_aoi_id)
                         super(FrmProfile, self).accept()
                 except Exception as ex:
                     try:
                         self.profile.delete(self.qris_project.project_file)
                     except Exception as ex_delete:
-                        QtWidgets.QMessageBox.warning(self, 'Error Importing Profile Features', str(ex_delete))
-                    QtWidgets.QMessageBox.warning(self, 'Error Importing Profile Features', str(ex))
+                        # add to message bar and log
+                        iface.messageBar().pushMessage('Error Deleting Profile', str(ex_delete), level=Qgis.Critical, duration=5)
+                        QgsApplication.messageLog().logMessage(f'Error Deleting Profile: {str(ex_delete)}', 'QRIS')
+                    # add to message bar and log
+                    iface.messageBar().pushMessage('Error Importing Profile Features', str(ex), level=Qgis.Critical, duration=5)
+                    QgsApplication.messageLog().logMessage(f'Error Importing Profile Features: {str(ex)}', 'QRIS', level=Qgis.Critical)
                     return
 
     def on_import_complete(self, result: bool):
@@ -141,7 +135,9 @@ class FrmProfile(QtWidgets.QDialog):
             try:
                 self.profile.delete(self.qris_project.project_file)
             except Exception as ex:
-                print(f'Error attempting to delete Profile after the importing of features failed.')
+                # add to message bar and log
+                iface.messageBar().pushMessage('Error Deleting Profile', str(ex), level=Qgis.Critical, duration=5)
+                QgsApplication.messageLog().logMessage(f'Error Deleting Profile: {str(ex)}', 'QRIS', level=Qgis.Critical)
             return
         else:
             iface.messageBar().pushMessage('Profile Import Complete.', f"{self.import_source_path} saved successfully.", level=Qgis.Info, duration=5)
@@ -160,30 +156,20 @@ class FrmProfile(QtWidgets.QDialog):
 
         self.grid = QtWidgets.QGridLayout()
 
-        self.lblName = QtWidgets.QLabel()
-        self.lblName.setText('Name')
+        self.lblName = QtWidgets.QLabel('Name')
         self.grid.addWidget(self.lblName, 0, 0, 1, 1)
 
         self.txtName = QtWidgets.QLineEdit()
         self.txtName.setMaxLength(255)
         self.grid.addWidget(self.txtName, 0, 1, 1, 1)
 
-        self.lblAttribute = QtWidgets.QLabel()
-        self.lblAttribute.setText('Attribute')
-        self.grid.addWidget(self.lblAttribute, 1, 0, 1, 1)
-
-        self.cboAttribute = QtWidgets.QComboBox()
-        self.grid.addWidget(self.cboAttribute, 1, 1, 1, 1)
-
-        self.lblMaskClip = QtWidgets.QLabel()
-        self.lblMaskClip.setText('Clip to AOI')
+        self.lblMaskClip = QtWidgets.QLabel('Clip to AOI')
         self.grid.addWidget(self.lblMaskClip, 2, 0, 1, 1)
 
         self.cboMaskClip = QtWidgets.QComboBox()
         self.grid.addWidget(self.cboMaskClip, 2, 1, 1, 1)
 
-        self.lblDescription = QtWidgets.QLabel()
-        self.lblDescription.setText('Description')
+        self.lblDescription = QtWidgets.QLabel('Description')
         self.grid.addWidget(self.lblDescription, 3, 0, 1, 1)
 
         self.txtDescription = QtWidgets.QPlainTextEdit()
@@ -196,9 +182,8 @@ class FrmProfile(QtWidgets.QDialog):
         # Metadata Tab
         self.tabs.addTab(self.metadata_widget, 'Metadata')
 
-        self.chkAddToMap = QtWidgets.QCheckBox()
+        self.chkAddToMap = QtWidgets.QCheckBox('Add to Map')
         self.chkAddToMap.setChecked(True)
-        self.chkAddToMap.setText('Add to Map')
         self.grid.addWidget(self.chkAddToMap, 4, 1, 1, 1)
 
         self.vert.addLayout(add_standard_form_buttons(self, 'profiles'))
