972a9ac2f2cc4f9e3f2fa1aaca542cb76bd91d68
Kelly W <kellymwhitehead@gmail.com>
2024-05-24 14:58:25 -0700

    Fix project export spatial views for symbology #405

--- src/view/frm_export_project.py
+++ src/view/frm_export_project.py
@@ -751,7 +751,7 @@ class FrmExportProject(QtWidgets.QDialog):
                     out_fields = '*'
                     if layer_fields is not None and len(layer_fields) > 0:
                         field_names = [field['label'] for field in layer_fields]
-                        out_fields = ", ".join([f'json_extract(metadata, \'$.{field}\') AS "{field}"' for field in field_names])
+                        out_fields = ", ".join([f'json_extract(metadata, \'$.attributes.{field}\') AS "{field}"' for field in field_names])
                     sql = f"CREATE VIEW {view_name} AS SELECT fid, geom, event_id, event_layer_id, {out_fields}, metadata FROM {fc_name} WHERE event_id == {event.id} AND event_layer_id == {layer.layer.id}"
                     self.create_spatial_view(view_name=view_name,
                                             fc_name=fc_name,
