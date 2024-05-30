011c8e29c3a03a81203a07fb2effbc341e8a03e6
Kelly W <kellymwhitehead@gmail.com>
2024-05-30 14:34:26 -0700

    Add a source dropdown in surfaces properties. #395

---
+++ src/db/migrations/017_raster_source_types.sql
@@ -0,0 +1,6 @@
+INSERT INTO lookup_list_values (list_name, list_value) VALUES ('raster_sources', 'Riverscapes Project');
+INSERT INTO lookup_list_values (list_name, list_value) VALUES ('raster_sources', 'Satellite');
+INSERT INTO lookup_list_values (list_name, list_value) VALUES ('raster_sources', 'Drone');
+INSERT INTO lookup_list_values (list_name, list_value) VALUES ('raster_sources', 'DroneDeploy');
+INSERT INTO lookup_list_values (list_name, list_value) VALUES ('raster_sources', 'Pleadies-Neo Satellite');
+INSERT INTO lookup_list_values (list_name, list_value) VALUES ('raster_sources', 'Derived');
