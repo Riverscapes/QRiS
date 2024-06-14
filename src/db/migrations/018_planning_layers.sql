76f1afe0fc7396d1cf7c30c1e845bc9672701cce
Kelly W <kellymwhitehead@gmail.com>
2024-06-14 15:10:08 -0700

    bug with planning layer schema

--- src/db/migrations/018_planning_layers.sql
+++ src/db/migrations/018_planning_layers.sql
@@ -5,7 +5,7 @@ INSERT INTO methods (id, name, machine_code) VALUES (7, 'Planning', 'PLANNING');
 INSERT INTO protocol_methods (protocol_id, method_id) VALUES (6, 7);
 
 INSERT INTO layers (id, fc_name, display_name, qml, is_lookup, geom_type, metadata) VALUES (40, 'recovery_potential', 'Recovery Potential', 'recovery_potential.qml', 0, 'Polygon', '{"hierarchy": ["Assessments"]}');
-INSERT INTO layers (id, fc_name, display_name, qml, is_lookup, geom_type, metadata) VALUES (41, 'risk_potential_points', 'Risk Potential', 'risk_potential_points.qml', 0, 'Point', '{"hierarchy": ["Assessments"], "fields": [{"machine_code": "risk_type", "label": "Risk", "type": "list", "lookup": "risk_type"}]}}');
+INSERT INTO layers (id, fc_name, display_name, qml, is_lookup, geom_type, metadata) VALUES (41, 'risk_potential_points', 'Risk Potential', 'risk_potential_points.qml', 0, 'Point', '{"hierarchy": ["Assessments"], "fields": [{"machine_code": "risk_type", "label": "Risk", "type": "list", "lookup": "risk_type"}]}');
 INSERT INTO layers (id, fc_name, display_name, qml, is_lookup, geom_type, metadata) VALUES (42, 'risk_potential_lines', 'Risk Potential', 'risk_potential_lines.qml', 0, 'Linestring', '{"hierarchy": ["Assessments"], "fields": [{"machine_code": "risk_type", "label": "Risk", "type": "list", "lookup": "risk_type"}]}');
 INSERT INTO layers (id, fc_name, display_name, qml, is_lookup, geom_type, metadata) VALUES (43, 'risk_potential_polygons', 'Risk Potential', 'risk_potential_polygons.qml', 0, 'Polygon', '{"hierarchy": ["Assessments"], "fields": [{"machine_code": "risk_type", "label": "Risk", "type": "list", "lookup": "risk_type"}]}');
 
