246454047fae229b368a3b1f5cd3dd300407842a
Matt Reimer <matt@northarrowresearch.com>
2024-09-06 15:30:14 -0700

    fixing the rsxml import

--- src/__init__.py
+++ src/__init__.py
@@ -1,18 +1,38 @@
+import re
+from qgis.core import QgsMessageLog, Qgis
 from ..__version__ import __version__
 
-RSXML_VERSION = '2.0.1'
+WHEEL_VERSION_PATTERN = r'.*rsxml-(\d+\.\d+\.\d+)-.*'
 
 # This is how we import the rsxml module. We do this because we want to bundle the rsxml whl with this package
 try:
-    import rsxml
-    print('rsxml imported from site-packages')
-except ImportError:
     import sys
     import os
-    this_dir = os.path.dirname(os.path.realpath(__file__))
-    path = os.path.join(this_dir, '..', 'wheels', f'rsxml-{RSXML_VERSION}-py3-none-any.whl')
-    sys.path.append(path)
-    if not os.path.exists(path):
-        raise Exception('rsxml wheel not found at {}.'.format(path))
+    wheels_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'wheels'))
+    # Find the wheel file
+    wheel_file = [f for f in os.listdir(wheels_dir) if f.startswith('rsxml-') and f.endswith('.whl')][0]
+    if not wheel_file:
+        QgsMessageLog.logMessage(f'Error loading rsxml from wheel file: no wheel file found in {wheels_dir}', 'QRis', Qgis.Critical)
+        raise Exception(f'rsxml wheel not found in {wheels_dir}.')
+    wheel_path = os.path.join(wheels_dir, wheel_file)
+    wheel_version = re.match(WHEEL_VERSION_PATTERN, wheel_file).group(1)
+
+    QgsMessageLog.logMessage(f'Found rsxml wheel file from {wheel_file} at version {str(wheel_version)}', 'QRis', Qgis.Info)
+
+    # Remove any existing rsxml module from sys.modules
+    if 'rsxml' in sys.modules:
+        QgsMessageLog.logMessage('Removing rsxml from sys.modules', 'QRis', Qgis.Info)
+        del sys.modules['rsxml']
+
+    if wheel_path not in sys.path:
+        QgsMessageLog.logMessage(f'Adding {wheel_path} to sys.path', 'QRis', Qgis.Info)
+        # We add the path to sys.path so that we can import the module
+        sys.path.insert(0, wheel_path)
+
     import rsxml
-    print('rsxml imported from wheels')
+    QgsMessageLog.logMessage(f'rsxml imported from wheel: {rsxml.__path__}', 'QRis', Qgis.Info)
+
+
+except ImportError:
+    QgsMessageLog.logMessage('Error importing rsxml from wheels', 'QRis', Qgis.Critical)
+    raise Exception('Error importing rsxml from wheels')
--- src/lib/environment.py
+++ src/lib/environment.py
@@ -3,10 +3,16 @@
 # https://stackoverflow.com/questions/40216311/reading-in-environment-variables-from-an-environment-file
 
 import os
+from qgis.core import QgsMessageLog, Qgis
 
-env_file = os.path.join(os.path.dirname(__file__), '..','..', '.env')
+env_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '.env'))
+
 
 def load_env_vars():
+    if not os.path.exists(env_file):
+        QgsMessageLog.logMessage(f'Environment file not found at {env_file}', 'QRis', Qgis.Warning)
+        return
+
     with open(env_file, 'r') as fh:
         vars_dict = dict(
             tuple(line.replace('\n', '').split('='))
