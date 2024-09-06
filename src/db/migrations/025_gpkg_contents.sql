223b9a6bdf64f4084895d13c061e7c74f600655b
Kelly W <kellymwhitehead@gmail.com>
2024-09-05 17:11:36 -0700

    add valley bottom and time series to gpkg contents

---
+++ src/db/migrations/025_gpkg_contents.sql
@@ -0,0 +1,5 @@
+INSERT INTO gpkg_contents (table_name, data_type, identifier, srs_id) VALUES ('planning_container_events', 'attributes', 'planning_container_events', 0);
+INSERT INTO gpkg_contents (table_name, data_type, identifier, srs_id) VALUES ('planning_containers', 'attributes', 'planning_containers', 0);
+INSERT INTO gpkg_contents (table_name, data_type, identifier, srs_id) VALUES ('sample_frame_time_series', 'attributes', 'sample_frame_time_series', 0);
+INSERT INTO gpkg_contents (table_name, data_type, identifier, srs_id) VALUES ('time_series', 'attributes', 'time_series', 0);
+INSERT INTO gpkg_contents (table_name, data_type, identifier, srs_id) VALUES ('valley_bottoms', 'attributes', 'valley_bottoms', 0);
