cf8240732fb4c686e9f171c24a5d4efad3635a92
Kelly W <kellymwhitehead@gmail.com>
2025-05-14 11:34:38 -0700

    initial climate engine qtask

--- src/view/frm_climate_engine_download.py
+++ src/view/frm_climate_engine_download.py
@@ -1,18 +1,16 @@
-
-import json
-import sqlite3
 import webbrowser
-import pandas as pd
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QDate
 
+from qgis.core import QgsApplication
 from qgis.utils import iface
 
 from .widgets.sample_frames import SampleFrameWidget
 from .widgets.date_range import DateRangeWidget
 from .utilities import add_help_button
 
+from ..gp.download_climate_engine_task import DownloadClimateEngineTimeseriesTask
 from ..model.project import Project
 from ..model.sample_frame import SampleFrame
 from ..lib.climate_engine import get_datasets, get_dataset_variables, get_dataset_date_range, get_dataset_timeseries_polygon, open_climate_engine_website
@@ -92,7 +90,9 @@ class FrmClimateEngineDownload(QtWidgets.QDialog):
 
     def retrieve_timeseries(self):
 
-        self.lblStatus.setText('Starting Metric Download...')
+        if self.txtName.text() == '':
+            QtWidgets.QMessageBox.warning(self, 'Error', 'Enter a name for the timeseries')
+            return
 
         if self.sample_frame_widget.selected_sample_frame() is None:
             QtWidgets.QMessageBox.warning(self, 'Error', 'Select a sample frame')
@@ -114,6 +114,8 @@ class FrmClimateEngineDownload(QtWidgets.QDialog):
                 if result == QtWidgets.QMessageBox.No:
                     return
 
+        self.lblStatus.setText('Starting Metric Download...')
+
         variables = {}
         for i in range(self.lboxVariables.count()):
             item = self.lboxVariables.item(i)
@@ -124,58 +126,24 @@ class FrmClimateEngineDownload(QtWidgets.QDialog):
             QtWidgets.QMessageBox.warning(self, 'Error', 'Select at least one variable')
             return
 
+        name = self.txtName.text()
         dataset = self.datasets.get(self.cboDataset.currentText())['id']
         start_date, end_date = self.date_range_widget.get_date_range()
-        status = True
-        time_series_ids = {}
 
-        for feature in self.sample_frame_widget.get_selected_sample_frame_features():
-            geometry = feature.geometry()
-            feature_id = feature.id()
-            self.lblStatus.setText(f'Downloading timeseries for feature {feature_id}')
+        features = list(self.sample_frame_widget.get_selected_sample_frame_features())
 
-            result = get_dataset_timeseries_polygon(dataset, list(variables.keys()), start_date, end_date, geometry)        
+        geometry = features[0].geometry()
 
-            if result is None:
-                status = False
-                continue
+        data = get_dataset_timeseries_polygon(dataset, list(variables.keys()), start_date, end_date, geometry=geometry)
+        task = DownloadClimateEngineTimeseriesTask(self.qris_project, name, dataset, list(variables.keys()), start_date, end_date, features)
+        # DEBUG
+        task.run()
+        # PRODUCTION
+        # task.download_complete.connect(self.on_download_complete)
+        # QgsApplication.taskManager().addTask(task)
+        
 
-            [timeseries] = result
-            data = timeseries.get('Data', None)
-
-            if data is None:
-                status = False
-                continue
-
-            df = pd.DataFrame(data)
-
-            with sqlite3.connect(self.qris_project.project_file) as conn:
-                cursor = conn.cursor()
-                
-                for column in df.columns:
-                    if column == 'Date':
-                        continue
-                    splits = column.split(' (')
-                    if len(splits) == 1:
-                        variable = column
-                        units = ''                    
-                    else:    
-                        variable, units = splits 
-                        units = units.replace(')', '')
-                    df_values = df[['Date', column]]
-                    df_values = df_values.set_index('Date')
-                    values = list(df_values.itertuples(name=None))
-                    name = f'{dataset} {variable}'
-                    if name in time_series_ids:
-                        time_series_id = time_series_ids[name]
-                    else:   
-                        metadata = {'units': units, 'start_date': start_date.strftime('%Y-%m-%d'), 'end_date': end_date.strftime('%Y-%m-%d'), 'description': variables[variable]}
-                        metadata_json = json.dumps(metadata)
-                        cursor.execute('INSERT INTO time_series (name, source, url, metadata) VALUES (?, ?, ?, ?)', (name, 'Climate Engine', 'https://www.climateengine.org/', metadata_json))
-                        time_series_id = cursor.lastrowid
-                        time_series_ids[name] = time_series_id
-                    cursor.executemany('INSERT INTO sample_frame_time_series (sample_frame_fid, time_series_id, time_value, value) VALUES (?, ?, ?, ?)', [(feature_id, time_series_id, date, value) for date, value in values])
-
+    def on_download_complete(self, status):
         if status is True:
             self.lblStatus.setText('Timeseries retrieved successfully')
             QtWidgets.QMessageBox.information(self, 'Climate Engine Download', 'Climate Engine Timeseries retrieved successfully')
@@ -184,6 +152,7 @@ class FrmClimateEngineDownload(QtWidgets.QDialog):
                 self.close()
         else:
             self.lblStatus.setText('Error retrieving timeseries')
+
 
     def setupUi(self):
 
@@ -196,20 +165,26 @@ class FrmClimateEngineDownload(QtWidgets.QDialog):
         self.grid = QtWidgets.QGridLayout()
         self.vert.addLayout(self.grid)
 
+        self.lblName = QtWidgets.QLabel('Name')
+        self.grid.addWidget(self.lblName, 0, 0)
+
+        self.txtName = QtWidgets.QLineEdit()
+        self.grid.addWidget(self.txtName, 0, 1)
+
         self.lblSampleFrames = QtWidgets.QLabel('Sample Frames')
         self.lblSampleFrames.setAlignment(QtCore.Qt.AlignTop)
-        self.grid.addWidget(self.lblSampleFrames, 0, 0)
+        self.grid.addWidget(self.lblSampleFrames, 1, 0)
 
-        self.grid.addWidget(self.sample_frame_widget, 0, 1)
+        self.grid.addWidget(self.sample_frame_widget, 1, 1)
 
         self.lblDataset = QtWidgets.QLabel('Dataset')
-        self.grid.addWidget(self.lblDataset, 1, 0)
+        self.grid.addWidget(self.lblDataset, 2, 0)
 
         self.cboDataset = QtWidgets.QComboBox()
-        self.grid.addWidget(self.cboDataset, 1, 1)
+        self.grid.addWidget(self.cboDataset, 2, 1)
 
         self.vert_variables = QtWidgets.QVBoxLayout()
-        self.grid.addLayout(self.vert_variables, 2, 0)
+        self.grid.addLayout(self.vert_variables, 3, 0)
 
         self.lblVariable = QtWidgets.QLabel('Variables')
         self.lblVariable.setAlignment(QtCore.Qt.AlignTop)
@@ -227,7 +202,7 @@ class FrmClimateEngineDownload(QtWidgets.QDialog):
 
         self.lboxVariables = QtWidgets.QListWidget()
         self.lboxVariables.setMinimumHeight(200)
-        self.grid.addWidget(self.lboxVariables, 2, 1)
+        self.grid.addWidget(self.lboxVariables, 3, 1)
 
         self.lblDateRange = QtWidgets.QLabel('Date Range')
         self.grid.addWidget(self.lblDateRange, 4, 0)
