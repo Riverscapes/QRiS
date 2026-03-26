efa8589021b2aeeccf16c5bf55b223a95a5ebe59
Kelly W <kellymwhitehead@gmail.com>
2026-03-26 15:29:17 -0700

    wrong version in about dialog #985

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 try:
-    from .__version__ import __version__
+    from ..__version__ import __version__
 except ImportError:
     # During tests, relative imports might fail if package structure is confused
     __version__ = "0.0.0dev"
