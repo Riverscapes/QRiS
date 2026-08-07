4ef40906998ae6d0d7b5935dc0a9a5de7f91a54e
Kelly W <kellymwhitehead@gmail.com>
2026-08-06 17:15:41 -0700

    deploy and checklist updates

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,55 +1,53 @@
-import os
-import sys
 from glob import glob
-import shutil
+import os
 import re
-import zipfile
-
+import shutil
+import sys
 
 PLUGIN_NAME = "qris_deploy"
 UI_DIR = "src/ui"
 
 # Environment variable that specifies the path to the QGIS plugins folder
 #  where the plugin will be deloyed.
-PLUGIN_ENV_VAR_NAME = 'QGIS_PLUGINS'
+PLUGIN_ENV_VAR_NAME = "QGIS_PLUGINS"
 
 
 def copy_plugin():
-    rootdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    rootdir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     deployfolder = os.path.abspath(os.path.join(os.getenv(PLUGIN_ENV_VAR_NAME), PLUGIN_NAME))
 
     if rootdir == deployfolder:
-        print('deploy and source folders cannot be the same!')
+        print("deploy and source folders cannot be the same!")
         sys.exit(1)
 
-    print('Deploy to {}? (y/N)'.format(deployfolder))
-    yesno('Exiting')
+    print(f"Deploy to {deployfolder}? (y/N)")
+    yesno("Exiting")
 
     if os.path.isdir(deployfolder):
-        print('Folder exists \n{}\n and will be deleted? Is this ok? (y/N)'.format(deployfolder))
-        yesno('Go change the __version__.py file and come back')
+        print(f"Folder exists \n{deployfolder}\n and will be deleted? Is this ok? (y/N)")
+        yesno("Go change the __version__.py file and come back")
         shutil.rmtree(deployfolder)
 
     os.mkdir(deployfolder)
     keep_patterns = [
-        ['__version__.py'],
-        ['icon.png'],
-        ['__init__.py'],
-        ['CHANGELOG.md'],
-        ['README.md'],
-        ['LICENSE'],
-        ['config.json'],
-        ['secrets.json'],
-        ['wheels', '**', '*.whl'],
-        ['src', '**', '*.py'],
-        ['src', '**', '*.json'],
-        ['src', '**', '*.css'],
-        ['src', '**', '*.html'],
-        ['src', '**', '*.sql'],
-        ['src', '**', '*.qml'],
-        ['resources', '**', 'us_states.gpkg'],
-        ['resources', '**', '*.json'],
-        ['resources', '**', '*.geojson']
+        ["__version__.py"],
+        ["icon.png"],
+        ["__init__.py"],
+        ["CHANGELOG.md"],
+        ["README.md"],
+        ["LICENSE"],
+        ["config.json"],
+        ["secrets.json"],
+        ["wheels", "**", "*.whl"],
+        ["src", "**", "*.py"],
+        ["src", "**", "*.json"],
+        ["src", "**", "*.css"],
+        ["src", "**", "*.html"],
+        ["src", "**", "*.sql"],
+        ["src", "**", "*.qml"],
+        ["resources", "**", "us_states.gpkg"],
+        ["resources", "**", "*.json"],
+        ["resources", "**", "*.geojson"],
     ]
     files = []
     for p in keep_patterns:
@@ -61,29 +59,29 @@ def copy_plugin():
         if not os.path.isdir(dst_dir):
             os.makedirs(dst_dir, exist_ok=True)
         shutil.copy(f, dst)
-        print('\n{}\n{}\n'.format(f, dst))
+        print(f"\n{f}\n{dst}\n")
 
     return deployfolder
 
 
 def move_meta(deployfolder, version):
     # Metadata must be handled separately
-    src = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'metadata.txt'))
-    dst = os.path.abspath(os.path.join(deployfolder, 'metadata.txt'))
+    src = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "metadata.txt"))
+    dst = os.path.abspath(os.path.join(deployfolder, "metadata.txt"))
 
-    with open(src, 'r', encoding="utf8") as f, open(dst, 'w+', encoding="utf8") as wf:
+    with open(src, encoding="utf8") as f, open(dst, "w+", encoding="utf8") as wf:
         text = f.read()
-        text = text.replace(' DEV_COPY', '')
-        text = text.replace('version=0.0.0dev', 'version={}'.format(version))
+        text = text.replace(" DEV_COPY", "")
+        text = text.replace("version=0.0.0dev", f"version={version}")
         wf.write(text)
 
 
 def zip_plugin(deployfolder: str, version: str):
     # ziph is zipfile handle
     root_dir = os.path.dirname(deployfolder)
-    zipfile_name = '{}-{}'.format(PLUGIN_NAME, version.replace('.', '_'))
+    zipfile_name = "{}-{}".format(PLUGIN_NAME, version.replace(".", "_"))
     zipfile_path = os.path.join(root_dir, zipfile_name)
-    shutil.make_archive(zipfile_path, 'zip', root_dir=root_dir, base_dir=PLUGIN_NAME)
+    shutil.make_archive(zipfile_path, "zip", root_dir=root_dir, base_dir=PLUGIN_NAME)
 
 
 def deploy_plugin():
@@ -97,19 +95,15 @@ def yesno(msg):
         sys.exit(1)
 
 
-if __name__ == '__main__':
-    print('Did the UI compile and you forgot to run "pb_tool compile" (y/N)')
-    yesno('run "pb_tool compile" and then try again')
+if __name__ == "__main__":
+    print('Did you run "Compile Resources" (y/N)')
+    yesno('Run "Compile Resources" and then try again')
 
-    vfile = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '__version__.py'))
-    version = re.search(
-        '^__version__\\s*=\\s*"(.*)"',
-        open(vfile).read(),
-        re.M
-    ).group(1)
+    vfile = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "__version__.py"))
+    version = re.search('^__version__\\s*=\\s*"(.*)"', open(vfile).read(), re.M).group(1)
 
-    print('Current version is: {}. Is this ok? (y/N)'.format(version))
-    yesno('Go change the __version__.py file and come back')
+    print(f"Current version is: {version}. Is this ok? (y/N)")
+    yesno("Go change the __version__.py file and come back")
 
     deployfolder = copy_plugin()
 
