c43d8f3225e6d195d254290d05bedca3931201aa
Philip <philip@northarrowresearch.com>
2026-09-22 13:56:52 -0700

    layout page progress

--- src/view/nwp27_widgets/LayoutPage.py
+++ src/view/nwp27_widgets/LayoutPage.py
@@ -1,5 +1,8 @@
 import os
 
+from qgis.core import (
+    QgsProject,
+)
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import (
     QHBoxLayout,
@@ -8,16 +11,9 @@ from qgis.PyQt.QtWidgets import (
     QVBoxLayout,
 )
 
-from ...lib.layout_tools import open_layout
+from ...lib.layout_tools import _get_layout_by_id, open_layout, serialize_layout
 from .BaseWidget import BaseWidget
-from .WizardStatus import STEP_UNKNOWN
-
-LAYOUTS = [
-    ("Project Elements Layout", "Description for Layout 1", "Design Template (Kelly)"),
-    ("Habitat Zones & Conditions", "Description for Layout 2", "machine2"),
-    ("Project Summary", "Description for Layout 3", "machine3"),
-    ("Reach Elements & Expected Outcomes", "Description for Layout 3", "machine4"),
-]
+from .WizardStatus import PRODUCTS, STEP_UNKNOWN
 
 
 class LayoutPage(BaseWidget):
@@ -37,16 +33,25 @@ class LayoutPage(BaseWidget):
             + " Each map is generated based on a specific layout template."
             + " Click the open buttons to view each of the layouts and manipulate them using QGIS native layout tools."
             + " When you are finished you must click the save button to preserve your changes."
-            + " You can print the layouts at this step, or wait and print them during the final package step at the end of the NWP27 process.\n"
+            + " You can print the layouts at this step, or wait and print them during the final package step at the end of the NWP27 process.\n\n"
+            + "You must click the save button. Closing the layout editor without clicking the save button below will lose your changes.\n"
         )
         label.setWordWrap(True)
         layout.addWidget(label)
 
-        for layout_name, description, machine_code in LAYOUTS:
+        for product in PRODUCTS:
+            name = product["name"]
+            description = product["description"]
+            machine_code = product["machine_code"]
+            is_layout = product["is_layout"]
+
+            if not is_layout:
+                continue
+
             h_layout = QHBoxLayout()
             layout.addLayout(h_layout)
 
-            name_label = QLabel(layout_name)
+            name_label = QLabel(name)
             h_layout.addWidget(name_label)
 
             description_label = QLabel(description)
@@ -58,25 +63,34 @@ class LayoutPage(BaseWidget):
             open_button.setToolTip("Open")
             open_button.setFixedWidth(self.BUTTON_WIDTH)
             open_button.setIcon(QIcon("/Users/philipbailey/code/riverscapes/nwp27-wizard/assets/open.svg"))
-            open_button.clicked.connect(lambda _, name=layout_name, code=machine_code: self.open_layout(name, code))
+            open_button.clicked.connect(lambda _, name=name, code=machine_code: self.open_layout(name, code))
             h_layout.addWidget(open_button)
 
             save_button = QPushButton()
             save_button.setToolTip("Save")
             save_button.setFixedWidth(self.BUTTON_WIDTH)
             save_button.setIcon(QIcon("/Users/philipbailey/code/riverscapes/nwp27-wizard/assets/save.svg"))
-            save_button.clicked.connect(lambda _, name=layout_name, code=machine_code: self.save_layout(name, code))
+            save_button.clicked.connect(lambda _, name=name, code=machine_code: self.save_layout(name, code))
             h_layout.addWidget(save_button)
 
             print_button = QPushButton()
             print_button.setToolTip("Print")
             print_button.setFixedWidth(self.BUTTON_WIDTH)
             print_button.setIcon(QIcon("/Users/philipbailey/code/riverscapes/nwp27-wizard/assets/print.svg"))
-            print_button.clicked.connect(lambda _, name=layout_name, code=machine_code: self.print_layout(name, code))
+            print_button.clicked.connect(lambda _, name=name, code=machine_code: self.print_layout(name, code))
             h_layout.addWidget(print_button)
 
         # stretch
         layout.addStretch()
+
+    def get_open_layout(self, name: str):
+        """
+        Retrieve the list of existing layout names in the current QGIS project.
+        """
+
+        qgis_project = QgsProject.instance()
+        layout_names = [layout.name() for layout in qgis_project.layoutManager().layouts()]
+        return layout_names
 
     def get_status(self) -> int:
         return STEP_UNKNOWN
@@ -91,13 +105,31 @@ class LayoutPage(BaseWidget):
         pass
 
     def open_layout(self, name: str, machine_code: str) -> None:
+
+        layout = _get_layout_by_id(machine_code)
+        if layout:
+            # Layout already exists in the project; focus its designer
+            QgsProject.instance().layoutManager().openDesigner(layout)
+            return
+
         layout_path = os.path.join(self.layouts_dir, f"{machine_code}.qpt")
         if os.path.exists(layout_path):
-            # Implement the logic to open the layout file
             open_layout(layout_path)
 
     def save_layout(self, name: str, machine_code: str) -> None:
-        pass
+
+        layout = _get_layout_by_id(machine_code)
+        if not layout:
+            return
+
+        current_layout_str = serialize_layout(layout)
+        existing_layouts = self.load_data("mapLayouts")
+        if existing_layouts is None:
+            existing_layouts = {}
+
+        existing_layouts[machine_code] = current_layout_str
+
+        self._save_data("mapLayouts", existing_layouts)
 
     def print_layout(self, name: str, machine_code: str) -> None:
         pass
--- src/view/nwp27_widgets/WizardStatus.py
+++ src/view/nwp27_widgets/WizardStatus.py
@@ -1,3 +1,11 @@
 STEP_INCOMPLETE = 0
 STEP_UNKNOWN = 1
 STEP_COMPLETE = 2
+
+PRODUCTS = [
+    {"name": "Project Elements Layout", "description": "Description for Layout 1", "machine_code": "Design Template (Kelly)", "is_layout": True},
+    {"name": "Habitat Zones & Conditions", "description": "Description for Layout 2", "machine_code": "machine2", "is_layout": True},
+    {"name": "Project Summary", "description": "Description for Layout 3", "machine_code": "machine3", "is_layout": True},
+    {"name": "Reach Elements & Expected Outcomes", "description": "Description for Layout 3", "machine_code": "machine4", "is_layout": True},
+    {"name": "JSON Export", "description": "Description for JSON Export", "machine_code": "json_export", "is_layout": False},
+]
